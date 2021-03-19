ef231eccf8894d9422a7930a1d13f4a12fcb0097
Markiian Sanotskyi <marksanockiy@gmail.com>
2021-03-19 15:10:13 -0400

    Done

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import FileUpload from './components/FileUpload';
+
+const App = () => <div className="container mt-4">
+  <h4 className="display-4 text-center mb-4">
+    <i className="fab fa-react" /> React File Upload
+  </h4>
+  <FileUpload />
+  </div>;
+
+export default App;
